322503ac080b9362f00d76dde0ce73beeb71f496
Dok511 <salmansalem611@gmail.com>
2026-05-28 06:46:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ if remaining_time > 0:
     user_name = st.text_input("اسم المزايد الكامل:", placeholder="اكتب اسمك هنا")
 
     # حساب أقل سومة مسموحة (السعر الحالي +5000$ زيادة)
-    min_next_bid = int(db["current_price"]) + 100
+    min_next_bid = int(db["current_price"]) + 5000
     bid_amount = st.number_input("قيمة مزاودتك ($):", min_value=min_next_bid, value=min_next_bid, step=5000)
 
     if st.button("🚀 اعتمد السومة لايف"):
